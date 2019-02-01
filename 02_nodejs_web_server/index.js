8c1cf80f4bb1c4e35c4c068e00578c1af29894bd
Dāvis Krēgers <kregers.davis@gmail.com>
2019-02-01 12:54:20 +0200

    Fix npm install cache invalidation

--- 02_nodejs_web_server/index.js
+++ 02_nodejs_web_server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hi there!');
+    res.send('Howdy!');
 });
 app.listen(8080, () => {
     console.log('Listening on port 8080');
