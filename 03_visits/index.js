add6fb7652755d018fd7617818d8bab6f54089e4
Dāvis Krēgers <kregers.davis@gmail.com>
2019-02-02 11:39:00 +0200

    Setup docker compose on visits counter

--- 03_visits/index.js
+++ 03_visits/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
