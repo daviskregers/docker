c7afdd0f12678d712a5412879461456be5645314
Dāvis Krēgers <kregers.davis@gmail.com>
2019-02-02 12:02:57 +0200

    add docker restart policy

--- 03_visits/index.js
+++ 03_visits/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const redis = require('redis');
+// const process = require('process');
 
 const app = express();
 const client = redis.createClient({
@@ -9,6 +10,7 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
+   // process.exit(0);
     client.get('visits', (err, visits) => {
         res.send('Visits: ' + visits);
         client.set('visits', parseInt(visits) + 1);
